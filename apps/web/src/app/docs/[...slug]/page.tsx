47607c308da8f57b582adb96eaeb9ffd8398e48f
rafighajili <rafig.hajili@gmail.com>
2026-01-16 15:03:03 +0100

    fix

--- apps/web/src/app/docs/[...slug]/page.tsx
+++ apps/web/src/app/docs/[...slug]/page.tsx
@@ -3,6 +3,8 @@ import { allDocs } from "contentlayer/generated";
 import { capitalize } from "inflection";
 import { notFound } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export async function generateMetadata({ params }: { params: Promise<{ slug?: string[] }> }) {
   const { slug = [] } = await params;
   const doc = allDocsSorted.find((d) => d.slug === slug.join("/"));
